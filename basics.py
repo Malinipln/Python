ca62f5322f607de1cfc81b4c9e42a44bd722c754
Malinipln <150351757+Malinipln@users.noreply.github.com>
2023-12-12 11:15:34 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,101 @@
+#name="cool"
+#print("my brother name:",name)
+
+a=10
+b=10
+c=a+b
+print('the total value is:',c)
+# float string
+# a=4.3
+# b=2.8
+# print(a-b)
+#
+# x=9
+# if x>10:
+#    print("i am greater than 10")
+# else:
+#     print("i am lesser than 10")
+
+
+# # # x=13
+# # # if x<10:
+# # #     print("i am greater than 10")
+# # # else:
+# # #     print("i am less than 10")
+# # #
+# # # total=[1,2,3,4,5,6,7,8,9,10]
+# # # entire_totl=sum(total)
+# # # print(entire_totl)
+# # #
+# # #
+# # # total=0
+# # # elements=[1,2,3,4,5,6,7,8,9,10]
+# # # for i in range(0, len(elements)):
+# # #     total=total+i
+# # # print(total)
+# # #
+# # #
+# #list
+# a = [5,10,15,20,25,30,35,40]
+#
+#
+# print("a[2] = ", a[2])
+#
+# #
+# print("a[0:3] = ", a[0:3])
+#
+#
+# print("a[5:] = ", a[5:])
+# #assigning new value to first index, since list can be changeable
+# a[6]=70
+# print(a)
+#
+# # #Tuple- it can not be modified
+# tuple1=("apple", "banana", "grapes")
+# print(tuple1[1])
+# # tuple1[2] = 'fruits'
+# # print(tuple1)
+#
+#
+#
+#
+#  #dictionary -each item pair in the form of key :value
+# # Changing and adding Dictionary Elements
+# my_dict = {'name': 'Jack', 'age': 26}
+# print(my_dict)
+# my_dict['age'] = 27
+# print(my_dict)
+#
+# my_dict['address'] = 'Downtown'
+#
+# print(my_dict)
+#
+
+
+
+
+
+
+
+
+# # # #set- it doesnot allow duplicates
+# # # # set of integers
+# my_set = {1, 2, 3,2,5}
+# print(my_set)
+# # #
+# # # # set of mixed datatypes
+# # # my_set = {1.0, "Hello", (1, 2, 3)}
+# # # print(my_set)
+# # # my_set = {1, 2, 3, 4, 3, 2}
+# # # print(my_set)
+# # #
+# # # # we can make set from a list
+# # # # Output: {1, 2, 3}
+# # # my_set = set([1, 2, 3, 2])
+# # # print(my_set)
+
+# x=9897
+# if x%2==0:
+#     print("the given number is even:",x)
+# else:
+#     print("the given number is odd:",x)
